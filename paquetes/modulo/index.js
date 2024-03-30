218956ff365ea70ca826271a6889c01f1e83b8aa
Andres Minota <andresnotami@gmail.com>
2024-03-29 22:28:49 -0500

    feat: :sparkles: Construyendo módulos

---
+++ paquetes/modulo/index.js
@@ -0,0 +1,7 @@
+// Traer nuestro modulo;
+var modulo = require("./modulo");
+
+// Ejecutar una funcion de nuestro modulo
+console.log(modulo.prop1);
+
+modulo.saludar();
---
+++ paquetes/modulo/modulo.js
@@ -0,0 +1,8 @@
+function saludar() {
+  console.log("Hola desde el módulo!");
+}
+
+module.exports = {
+  saludar,
+  prop1: "Hola que tal",
+};
